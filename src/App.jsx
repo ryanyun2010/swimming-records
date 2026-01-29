af07671a4e1c142180e1f9d8c25fd7ca9c73f30a
Ryan <ryanyun2010@gmail.com>
2026-01-29 15:34:28 -0800

    path fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import AdminPage from "pages/AdminPage"; // your AdminPage component
+import AdminPage from "./pages/AdminPage"; // your AdminPage component
 import { formatEventLabel, formatDate } from "lib/utils";
 
 // --- Home Page ---
