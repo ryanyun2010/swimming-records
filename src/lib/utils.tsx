d6beb30d25dd18aa9aecb5bf591cc03bebec6b45
Ryan Yun <ryan@Ryans-MacBook-Air-2.local>
2026-02-08 19:03:41 -0800

    remove safeguard

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -77,9 +77,6 @@ export function getResponseJSONAndParse(
 	schema: z.ZodSchema<any>,
 	errFunc: (errMsg: string) => ErrorRes = (e: string) => new Errors.MalformedResponse(`Failed to parse response JSON: ${e}`),
 ): ResultAsync<any, ErrorRes> {
-	if (!(response instanceof Response)) {
-		return errAsync(errFunc("response not of type Response"));
-	}
 	return getResponseJSON(response, errFunc).andThen(
 		zodParseWith(schema, errFunc)
 	);
